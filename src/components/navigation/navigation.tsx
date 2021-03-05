70d126c664794c6ddbccc942bf07216c5b30e618
Ondrej Hepnar <ondrej.hepnar@gmail.com>
2021-03-05 21:29:45 +0100

    create initial nav component

---
+++ src/components/navigation/navigation.tsx
@@ -0,0 +1,7 @@
+import logo from './iwik-logo.png'
+
+export default function Navigation() {
+	return <header className="">
+		<img src={logo} className="" alt="logo" width={55} height={60} />
+	</header>
+}
